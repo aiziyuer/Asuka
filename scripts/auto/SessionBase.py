aad5c0f803f15a9eab5494be1f6360375ca3b5c7
lc <ziyu0123456789@gmail.com>
2016-05-30 02:00:15 +0800

    测试豆瓣页面登录功能

--- scripts/auto/SessionBase.py
+++ scripts/auto/SessionBase.py
@@ -6,10 +6,10 @@
 """
 
 from mechanize import Browser
+import cookielib
 
 
 class SessionBase:
-
     # 版本信息说明
     __author__ = 'aiziyuer'
     __version__ = "1.0.1"
@@ -22,9 +22,33 @@ class SessionBase:
     __status__ = 'Developing'
 
     # 登录的URL TODO 后面需要做到配置文件里面去
-    LOGIN_URL = 'http://www.douban.com'
+    LOGIN_URL = 'http://www.douban.com/accounts/login'
+
     # 新建一个浏览器Agent
     br = Browser()
+
+    # 设置代理
+    br.set_proxies({"http": "127.0.0.1:8888"})
+
+    # Cookie Jar
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
+
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+
+    # Want debugging messages?
+    # br.set_debug_http(True)
+    # br.set_debug_redirects(True)
+    # br.set_debug_responses(True)
+
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent',
+                      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:46.0) Gecko/20100101 Firefox/46.0')]
 
     def __init__(self):
         self.read_config()
@@ -35,7 +59,14 @@ class SessionBase:
         print 'read_config' + self.__author__
 
     def try_login(self):
+        br = self.br
+
         # 请求登录也
-        self.br.follow_link(self.LOGIN_URL)
+        br.open(self.LOGIN_URL)
+        br.select_form(name='lzform')
+        br['form_email'] = 'ziyu0123456789@gmail.com'
+        br['form_password'] = ''
+        resp = br.submit()
+        print resp.read()
 
         # TODO 填写表单
